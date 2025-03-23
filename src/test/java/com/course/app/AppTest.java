b6bbb7649852f8fb5e5b6c48154a54ac97eb577a
Tyoma-Pi <artyom.pischulin@gmail.com>
2025-03-23 22:21:13 +0500

    Upd: .jar file works and new classes

--- src/test/java/com/course/app/AppTest.java
+++ src/test/java/com/course/app/AppTest.java
@@ -1,19 +1,18 @@
 package com.course.app;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest {
-
-    /**
-     * Rigorous Test :-)
-     */
     @Test
     public void shouldAnswerWithTrue() {
         assertTrue(true);
+    }
+
+    @Test
+    public void shouldAnswerWithFalse() {
+        assertFalse(false);
     }
 }
