31f1e3e5c8ad89ba6affbddc84582800571c93ba
artyom.pischulin@gmail.com <>
2025-03-31 17:06:09 +0500

    Update: clean project draft

--- src/main/java/com/course/app/App.java
+++ src/main/java/com/course/app/App.java
@@ -1,10 +1,7 @@
 package com.course.app;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello, world!");
     }
 }
